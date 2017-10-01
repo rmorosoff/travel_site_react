ad909dfe13c81520bf6e01ca044a6c62c906628b
Rudy Morosoff <rudy.morosoff@libertymutual.com>
2017-10-01 15:35:50 -0700

    Updated class to className in footer component.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,19 +4,19 @@ import './Footer.css';
 class Footer extends Component {
   render() {
     return (
-        <nav class="navbar navbar-inverse navbar-fixed-bottom">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
+        <nav className="navbar navbar-inverse navbar-fixed-bottom">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
                         </button>
-                    <a class="navbar-brand" href="#">Travel is Fun</a>
+                    <a className="navbar-brand" href="#">Travel is Fun</a>
                 </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav navbar-right">
                     <li><a href="#">Facebook Link</a></li>
                     </ul>
                 </div>
